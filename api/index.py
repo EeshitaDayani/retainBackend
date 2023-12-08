66cec2a6908358f699c0b356d2d2e1567e89c1b6
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-07 23:12:10 -0500

    Uncommented code

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from PIL import Image
-#import pytesseract
-# import easyocr
-#from pydub import AudioSegment
-# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
-# import speech_recognition as sr
+from PIL import Image
+import pytesseract
+import easyocr
+from pydub import AudioSegment
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
+import speech_recognition as sr
 import requests
 
 
@@ -24,57 +24,57 @@ def return_text_input():
         'message': referenceText
     })
 
-# @app.route("/api/extractTextFromImage", methods=['POST'])
-# def extract_text_image():
-#     global referenceText
-#     file = request.files['file']
-#     # Save the uploaded image temporarily
-#     image_path = 'temp_image.png'
-#     file.save(image_path)
+@app.route("/api/extractTextFromImage", methods=['POST'])
+def extract_text_image():
+    global referenceText
+    file = request.files['file']
+    # Save the uploaded image temporarily
+    image_path = 'temp_image.png'
+    file.save(image_path)
 
-#     # Use Tesseract to extract text from the image
-#     reader = easyocr.Reader(['en'], gpu = True)
-#     referenceText = (' '.join(reader.readtext(image_path, detail = 0)))
-#     #referenceText = pytesseract.image_to_string(Image.open(image_path)).strip()
+    # Use Tesseract to extract text from the image
+    reader = easyocr.Reader(['en'], gpu = True)
+    referenceText = (' '.join(reader.readtext(image_path, detail = 0)))
+    #referenceText = pytesseract.image_to_string(Image.open(image_path)).strip()
 
-#     # Remove the temporary image file
-#     import os
-#     os.remove(image_path)
+    # Remove the temporary image file
+    import os
+    os.remove(image_path)
 
-#     return jsonify({
-#         'text': referenceText
-#     })
+    return jsonify({
+        'text': referenceText
+    })
 
-# @app.route("/api/extractTextFromAudio", methods=['POST'])
-# def extract_text_from_audio():
-#     global referenceText
-#     audio_file = request.files['audio']
-#     # Save the uploaded audio temporarily
-#     audio_path = 'temp_audio.webm'
-#     audio_file.save(audio_path)
+@app.route("/api/extractTextFromAudio", methods=['POST'])
+def extract_text_from_audio():
+    global referenceText
+    audio_file = request.files['audio']
+    # Save the uploaded audio temporarily
+    audio_path = 'temp_audio.webm'
+    audio_file.save(audio_path)
 
-#     # Convert the webm format to wav using pydub
-#     #sound = AudioSegment.from_file(audio_path, format="webm")
-#     #sound.export("temp_audio.wav", format="wav")
-#     ffmpeg_extract_audio(audio_path, "temp_audio.wav")
-#     #sound = moviepy.VideoFileClip(audio_path)
-#     #sound.audio.write_audiofile("temp_audio.wav")
+    # Convert the webm format to wav using pydub
+    #sound = AudioSegment.from_file(audio_path, format="webm")
+    #sound.export("temp_audio.wav", format="wav")
+    ffmpeg_extract_audio(audio_path, "temp_audio.wav")
+    #sound = moviepy.VideoFileClip(audio_path)
+    #sound.audio.write_audiofile("temp_audio.wav")
 
-#     # Use SpeechRecognition to transcribe the audio
-#     recognizer = sr.Recognizer()
-#     with sr.AudioFile("temp_audio.wav") as source:
-#         audio_data = recognizer.record(source)
-#         extracted_text = recognizer.recognize_google(audio_data, show_all=True)
-#         referenceText = extracted_text['alternative'][0]['transcript']
+    # Use SpeechRecognition to transcribe the audio
+    recognizer = sr.Recognizer()
+    with sr.AudioFile("temp_audio.wav") as source:
+        audio_data = recognizer.record(source)
+        extracted_text = recognizer.recognize_google(audio_data, show_all=True)
+        referenceText = extracted_text['alternative'][0]['transcript']
 
-#         # Remove the temporary audio files
-#         import os
-#         os.remove(audio_path)
-#         os.remove("temp_audio.wav")
+        # Remove the temporary audio files
+        import os
+        os.remove(audio_path)
+        os.remove("temp_audio.wav")
 
-#         return jsonify({
-#             'text': referenceText
-#         })
+        return jsonify({
+            'text': referenceText
+        })
 
 
 @app.route("/api/extractedText", methods=['GET'])
@@ -84,37 +84,37 @@ def extractedText():
     })
 
 
-# @app.route("/api/getUserAttempt", methods=['POST'])
-# def extract_user_input():
-#     global userInput
-#     audio_file = request.files['audio']
-#     # Save the uploaded audio temporarily
-#     audio_path = 'temp_audio.webm'
-#     audio_file.save(audio_path)
+@app.route("/api/getUserAttempt", methods=['POST'])
+def extract_user_input():
+    global userInput
+    audio_file = request.files['audio']
+    # Save the uploaded audio temporarily
+    audio_path = 'temp_audio.webm'
+    audio_file.save(audio_path)
 
-#     # Convert the webm format to wav using pydub
-#     # sound = AudioSegment.from_file(audio_path, format="webm")
-#     # sound.export("temp_audio.wav", format="wav")
-#     ffmpeg_extract_audio(audio_path, "temp_audio.wav")
-#     # sound = moviepy.VideoFileClip(audio_path)
-#     # sound.audio.write_audiofile("temp_audio.wav")
+    # Convert the webm format to wav using pydub
+    # sound = AudioSegment.from_file(audio_path, format="webm")
+    # sound.export("temp_audio.wav", format="wav")
+    ffmpeg_extract_audio(audio_path, "temp_audio.wav")
+    # sound = moviepy.VideoFileClip(audio_path)
+    # sound.audio.write_audiofile("temp_audio.wav")
 
-#     # Use SpeechRecognition to transcribe the audio
-#     recognizer = sr.Recognizer()
-#     with sr.AudioFile("temp_audio.wav") as source:
-#         global userInput
-#         audio_data = recognizer.record(source)
-#         extracted_text = recognizer.recognize_google(audio_data, show_all=True)
-#         userInput = extracted_text['alternative'][0]['transcript']
+    # Use SpeechRecognition to transcribe the audio
+    recognizer = sr.Recognizer()
+    with sr.AudioFile("temp_audio.wav") as source:
+        global userInput
+        audio_data = recognizer.record(source)
+        extracted_text = recognizer.recognize_google(audio_data, show_all=True)
+        userInput = extracted_text['alternative'][0]['transcript']
 
-#         # Remove the temporary audio files
-#         import os
-#         os.remove(audio_path)
-#         os.remove("temp_audio.wav")
+        # Remove the temporary audio files
+        import os
+        os.remove(audio_path)
+        os.remove("temp_audio.wav")
 
-#         return jsonify({
-#             'text': userInput
-#         })
+        return jsonify({
+            'text': userInput
+        })
 
 @app.route("/api/compareTexts", methods=['GET'])
 def compareTexts():
